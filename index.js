be9abd78605216632552efcf4fc4a04fdcfa06e3
Harika <lakshmimaha683@gmail.com>
2023-04-15 22:09:16 +0530

    Updated CSS, added favicon

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import {
   ref,
   push,
   onValue,
+  remove,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
@@ -25,16 +26,22 @@ submitBtn.addEventListener("click", () => {
   console.log(`${inputValue} added to database`);
   // appendListElem(inputValue);
   clear();
+});
 
-  onValue(values, function (snapshot) {
+onValue(values, function (snapshot) {
+  if (snapshot.exists()) {
     // console.log(snapshot.val());
-    var itemsArray = Object.values(snapshot.val());
+    var itemsArray = Object.entries(snapshot.val());
     clearListEl();
     for (let i = 0; i < itemsArray.length; i++) {
-      appendListElem(itemsArray[i]);
-      console.log(itemsArray[i]);
+      var currentItem = itemsArray[i];
+      var currentItemID = currentItem[0];
+      var currentItemValue = currentItem[1];
+      appendListElem(currentItem);
     }
-  });
+  } else {
+    document.getElementById("shopList").innerHTML = "no more entries!";
+  }
 });
 
 function clearListEl() {
@@ -45,7 +52,19 @@ function clear() {
   input.value = "";
 }
 function appendListElem(inputVal) {
-  document
-    .getElementById("shopList")
-    .insertAdjacentHTML("afterbegin", `<li>${inputVal}</li>`);
+  // document
+  //   .getElementById("shopList")
+  //   .insertAdjacentHTML("afterbegin", `<li>${inputVal}</li>`);
+
+  let itemID = inputVal[0];
+  let itemValue = inputVal[1];
+  let newEl = document.createElement("li");
+  newEl.textContent = itemValue;
+
+  newEl.addEventListener("click", function () {
+    let itemLocation = ref(database, `items/${itemID}`);
+    remove(itemLocation);
+  });
+
+  document.getElementById("shopList").append(newEl);
 }
