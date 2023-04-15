8ca6ff4d72c703c3dc813baa7170c7f2a087d4bf
Harika <lakshmimaha683@gmail.com>
2023-04-15 14:04:19 +0530

    Uploaded files

---
+++ index.js
@@ -0,0 +1,40 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL:
+    "https://shoppinglist-35735-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const values = ref(database, "items");
+
+var input = document.getElementById("input");
+var submitBtn = document.getElementById("submitBtn");
+
+submitBtn.addEventListener("click", () => {
+  let inputValue = input.value;
+
+  push(values, inputValue);
+  console.log(`${inputValue} added to database`);
+  appendListElem(inputValue);
+  clear();
+
+  onValue(values, function (snapshot) {
+    console.log(snapshot.val());
+  });
+});
+function clear() {
+  input.value = "";
+}
+function appendListElem(inputVal) {
+  document
+    .getElementById("shopList")
+    .insertAdjacentHTML("afterbegin", `<li>${inputVal}</li>`);
+}
